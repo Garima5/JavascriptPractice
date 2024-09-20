ea71e505640383df3d2ea438a4b84415f77ebe41
Garima <garimaaggarwal55@gmail.com>
2024-09-20 06:54:25 -0400

    updates

--- RandomPractice/app.js
+++ RandomPractice/app.js
@@ -5,4 +5,33 @@ alert("OUCH! Stop poking me");
 document.querySelector('html').addEventListener('click',poke);
 */
 
-originalImage = document.querySelector("img")
+//originalImage = document.querySelector("img")
+
+// Fetch api
+url = "https://cat-fact.herokuapp.com/facts"
+
+
+buttonImg = document.querySelector("button")
+
+let newbtn = document.querySelector(".newbtn");
+console.log(newbtn)
+newbtn.onclick=(evt)=>{
+    //.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+}
+let currentMode = 'light';
+let togglebtn = document.querySelector(".togglebtn")
+togglebtn.addEventListener('click',()=>{
+console.log("You are trying to chaneg mode");
+if (currentMode==='light'){
+    currentMode='dark';
+    document.querySelector('body').style.backgroundColor='black';
+}
+else{
+    currentMode='light';
+    document.querySelector('body').style.backgroundColor='white';
+}
+console.log(currentMode);
+
+});
