629c41c742d92ad8750bc544a380a023df7d8f0c
Garima <garimaaggarwal55@gmail.com>
2024-09-24 05:17:36 -0400

    Counter app

--- counter/app.js
+++ counter/app.js
@@ -9,14 +9,18 @@ let resetVal = 0
 increase.addEventListener('click',()=>{
     resetVal = resetVal+1;
     counter.innerHTML = resetVal;
+    counter.style.color = 'green'
 })
 
 decrease.addEventListener('click',()=>{
     resetVal = resetVal-1;
     counter.innerHTML = resetVal;
+
+      counter.style.color = 'red'
 })
 
 reset.addEventListener('click',()=>{
 
     counter.innerHTML = 0;
+      counter.style.color = 'grey'
 })
